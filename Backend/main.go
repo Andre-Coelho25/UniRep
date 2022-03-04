17ada7d809d4a0f73e36ebc8d89ac214615af903
Andre-Coelho25 <andre_coelho25@hotmail.com>
2022-03-04 17:45:22 +0000

    bruh

--- Backend/main.go
+++ Backend/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+type User struct {
+	Id       int
+	Name     string
+	email    string
+	password string
+}
 
 func main() {
 	fmt.Println("Go MySQL Tutorial")
@@ -18,6 +26,22 @@ func main() {
 	// if there is an error opening the connection, handle it
 	if err != nil {
 		panic(err.Error())
+	}
+
+	results, err := db.Query("SELECT FROM user")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	for results.Next() {
+		var tag User
+		// for each row, scan the result into our tag composite object
+		err = results.Scan(&tag.Id, &tag.Name)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		// and then print out the tag's Name attribute
+		log.Printf(tag.Name)
 	}
 
 	// defer the close till after the main function has finished
